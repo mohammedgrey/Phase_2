f7d6ab4c888db4026818660e0ea040a18e2e6c4a
MoaazZaki <44510702+MoaazZaki@users.noreply.github.com>
2018-11-30 16:16:14 +0200

    Add files via upload

--- ApplicationManager.cpp
+++ ApplicationManager.cpp
@@ -4,6 +4,20 @@
 
 #include "Actions/AddFigureAction.h"
 #include "Actions/MainMenuAction.h"
+#include "Figures/CRectangle.h"
+#include "Figures/CLine.h"
+#include "Figures/CTriangle.h"
+#include "Figures/CRhombus.h"
+#include "Figures/CEllipse.h"
+
+#include <cmath>
+
+#include "Actions\AddLineAction.h"
+#include "Actions/AddEllipAction.h"
+#include "Actions/AddRhomAction.h"
+#include "Actions/AddTriAction.h"
+#include "Actions/SelectAction.h"
+
 
 
 
@@ -16,6 +30,7 @@ ApplicationManager::ApplicationManager()
 	
 	FigCount = 0;
 		
+	SelectedFig = NULL;
 	//Create an array of figure pointers and set them to NULL		
 	for(int i=0; i<MaxFigCount; i++)
 		FigList[i] = NULL;	
@@ -57,8 +72,23 @@ void ApplicationManager::ExecuteAction(ActionType ActType)
 			break;
 
 		case DRAW_LINE:
-			///create AddLineAction here
+			pAct = new AddLineAction(this);
+			break;
 
+		case DRAW_RHOMBUS:
+			pAct = new AddRhomAction(this);
+			break;
+
+		case DRAW_TRI:
+			pAct = new AddTriAction(this);
+			break;
+
+		case DRAW_ELLIPSE:
+			pAct = new AddEllipAction(this);
+			break;
+
+		case SELECT:
+			pAct = new SelectAction(this);
 			break;
 
 		case EXIT:
@@ -92,13 +122,53 @@ void ApplicationManager::AddFigure(CFigure* pFig)
 CFigure *ApplicationManager::GetFigure(int x, int y) const
 {
 	//If a figure is found return a pointer to it.
+	
+	int Type;
+	CFigure* Ptr;
+
+	for (int i = FigCount-1; i >= 0; i--) {
+
+		Type = FigList[i]->GetID();
+		Ptr = FigList[i];
+
+		switch (Type)
+		{
+		case 0:
+			if (InsideRect(FigList[i], x, y)) return FigList[i];
+			break;
+		case 1:
+			if (InsideLine(FigList[i], x, y)) return FigList[i];
+			break;
+		case 2:
+				if (InsideTri(FigList[i], x, y)) return FigList[i];
+			break;
+		case 3:
+				if (InsideRhom(FigList[i], x, y)) return FigList[i];
+				break;
+		case 4:
+				if (InsideEllip(FigList[i], x, y)) return FigList[i];
+				break;
+		}
+	}
+	
 	//if this point (x,y) does not belong to any figure return NULL
-
-
 	//Add your code here to search for a figure given a point x,y	
-	//Remember that ApplicationManager only calls functions do NOT implement it.
+	//Remember that ApplicationManager only calls functions do NOT implement it or you will simply get ZERO.
 
 	return NULL;
+}
+
+void ApplicationManager::SetSelectedFig(CFigure*a) {
+	SelectedFig = a;
+}
+
+CFigure *ApplicationManager::GetSelectedFig() {
+	return SelectedFig;
+}
+
+void ApplicationManager::SetClipboard(CFigure * C) {
+	Clipboard = C;
+
 }
 //==================================================================================//
 //							Interface Management Functions							//
